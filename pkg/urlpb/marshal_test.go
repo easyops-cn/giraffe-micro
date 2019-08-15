4ff4ca07fb21bfcc62764256d80b70cc5e0b2d69
cheuk.chiu <cheuk0.0chiu@gmail.com>
2019-08-16 00:27:15 +0800

    chore(urlpb): 改进单元测试

--- pkg/urlpb/marshal_test.go
+++ pkg/urlpb/marshal_test.go
@@ -3,15 +3,55 @@ package urlpb
 import (
 	"fmt"
 	"net/url"
+	"reflect"
 	"testing"
 
-	"github.com/go-test/deep"
 	"github.com/gogo/protobuf/proto"
-
-	"github.com/easyops-cn/giraffe-micro/pkg/urlpb/mock"
 )
 
-var queryData = &mock.Query{
+type structType struct {
+	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *structType) Reset()         { *m = structType{} }
+func (m *structType) String() string { return proto.CompactTextString(m) }
+func (*structType) ProtoMessage()    {}
+
+type queryMessage struct {
+	StringValue          string      `protobuf:"bytes,1,opt,name=string_value,json=stringValue,proto3" json:"string_value,omitempty"`
+	BoolValue            bool        `protobuf:"varint,2,opt,name=bool_value,json=boolValue,proto3" json:"bool_value,omitempty"`
+	Int32Value           int32       `protobuf:"varint,3,opt,name=int32_value,json=int32Value,proto3" json:"int32_value,omitempty"`
+	Int64Value           int64       `protobuf:"varint,4,opt,name=int64_value,json=int64Value,proto3" json:"int64_value,omitempty"`
+	Uint32Value          uint32      `protobuf:"varint,5,opt,name=uint32_value,json=uint32Value,proto3" json:"uint32_value,omitempty"`
+	Uint64Value          uint64      `protobuf:"varint,6,opt,name=uint64_value,json=uint64Value,proto3" json:"uint64_value,omitempty"`
+	FloatValue           float32     `protobuf:"fixed32,7,opt,name=float_value,json=floatValue,proto3" json:"float_value,omitempty"`
+	DoubleValue          float64     `protobuf:"fixed64,8,opt,name=double_value,json=doubleValue,proto3" json:"double_value,omitempty"`
+	StructValue          *structType `protobuf:"bytes,9,opt,name=struct_value,json=structValue,proto3" json:"struct_value,omitempty"`
+	SliceValue           []string    `protobuf:"bytes,10,rep,name=slice_value,json=sliceValue,proto3" json:"slice_value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}    `json:"-"`
+	XXX_unrecognized     []byte      `json:"-"`
+	XXX_sizecache        int32       `json:"-"`
+}
+
+func (m *queryMessage) Reset()         { *m = queryMessage{} }
+func (m *queryMessage) String() string { return proto.CompactTextString(m) }
+func (*queryMessage) ProtoMessage()    {}
+
+type sampleMessage struct {
+	Name                 string   `json:"name,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *sampleMessage) Reset()         { *m = sampleMessage{} }
+func (m *sampleMessage) String() string { return proto.CompactTextString(m) }
+func (*sampleMessage) ProtoMessage()    {}
+
+var queryData = &queryMessage{
 	StringValue: "abc",
 	BoolValue:   true,
 	Int32Value:  -123,
@@ -20,7 +60,7 @@ var queryData = &mock.Query{
 	Uint64Value: 10,
 	FloatValue:  1.0,
 	DoubleValue: 1.01,
-	StructValue: &mock.S{Name: "index"},
+	StructValue: &structType{Name: "index"},
 	SliceValue:  []string{"a", "b", "c"},
 }
 
@@ -55,14 +95,14 @@ func TestMarshal(t *testing.T) {
 		{
 			name: "TestNilProtoMessage",
 			args: args{
-				pb: (*mock.Query)(nil),
+				pb: (*queryMessage)(nil),
 			},
 			wantErr: true,
 		},
 		{
 			name: "TestWithoutProtobufTag",
 			args: args{
-				pb: &mock.SS{Name: "aa"},
+				pb: &sampleMessage{Name: "aa"},
 			},
 			want:    url.Values{},
 			wantErr: false,
@@ -75,11 +115,8 @@ func TestMarshal(t *testing.T) {
 				t.Errorf("Marshal() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			//if !reflect.DeepEqual(got, tt.want) {
-			//	t.Errorf("Marshal() = %v, want %v", got, tt.want)
-			//}
-			if diff := deep.Equal(got, tt.want); diff != nil {
-				t.Error(diff)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Marshal() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -161,7 +198,7 @@ func TestParseURL(t *testing.T) {
 			name: "TestWithNilProtoMessage",
 			args: args{
 				rawurl:     "http://127.0.0.1:8080/string/abc/bool/true?a=123",
-				pb:         (*mock.Query)(nil),
+				pb:         (*queryMessage)(nil),
 				parseQuery: true,
 			},
 			want:    "<nil>",
@@ -186,11 +223,8 @@ func TestParseURL(t *testing.T) {
 				return
 			}
 			gotString := fmt.Sprint(got)
-			//if !reflect.DeepEqual(gotString, tt.want) {
-			//	t.Errorf("ParseURL() = %v, want %v", got, tt.want)
-			//}
-			if diff := deep.Equal(gotString, tt.want); diff != nil {
-				t.Error(diff)
+			if !reflect.DeepEqual(gotString, tt.want) {
+				t.Errorf("ParseURL() = %v, want %v", got, tt.want)
 			}
 		})
 	}
